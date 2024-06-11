dea0eebaf5194c54dba5eb9ba92576d47b35e9aa
Anatoli Ryzhankou <a-ryzhankou@toloka.ai>
2024-06-11 10:58:58 +0300

    fix

--- src/Components/ReposCard.tsx
+++ src/Components/ReposCard.tsx
@@ -17,6 +17,7 @@ export const ReposCard = ({repo}: IProps) => {
         addFavourites(repo.html_url)
         setIsFav(true)
     }
+
     const removeFromFavorites = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         removeFavourites(repo.html_url)
